39a26cd5432988b0ee6e147df0b01b71db4ea249
lloydkev000 <lloydkevin631@gmail.com>
2025-02-14 13:53:27 -0500

    done

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-public class Main {
-}
+    public class Main {
+        public static void main(String[] args) {
+            Student mark = new Student("Mark", 3.87);
+            Student linda = new Student("Linda", 4.12);
+            Student clarence = new Student("Clarence", 3.93);
+
+            List<Student> students = new ArrayList<>();
+            students.add(mark);
+            students.add(linda);
+            students.add(clarence);
+
+            Collections.sort(students);
+            System.out.println(students);
+
+        }
+    }
--- src/Student.java
+++ src/Student.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+public class Student implements Comparable<Student> {
+    private String name;
+    private double gpa;
 
-public class Student {
+    public Student(String name, double age) {
+        this.name = name;
+        this.gpa = age;
+    }
+
+    @Override
+    public int compareTo(Student other) {
+        return -1 * Double.compare(gpa, other.gpa);
+    }
 }
